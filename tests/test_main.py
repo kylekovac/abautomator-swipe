b48f3bef90d1c2b6813f7279571437c884f16efd
Rubens Flam-Shepherd <ruben.flam-shepherd@citizen.com>
2021-12-22 23:44:16 -0500

    figured out how to ignore sqwarning

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,19 @@
-
+from datetime import date, timedelta
 from sqlalchemy import create_engine
 
 from abautomator import main
 
+
 def test_answer():
     engine = create_engine(f'bigquery://{main.GCP_PROJECT_ID}')
-    conn = engine.connect()
-    users = main.get_users(engine, main.exp_config)
+    test_config = main.exp_config
+    test_config["EXP_START"] = date.today() - timedelta(days = 1)
+    users = main.get_users(engine, test_config)
 
     print(users)
 
-    # result = conn.execute(users).all()
-    # print(len(result))
-    # print(result[0:10])
-    
-    assert True
+    conn = engine.connect()
+    result = conn.execute(users).all()
+    print(len(result))
+
+    assert len(result) > 1    
