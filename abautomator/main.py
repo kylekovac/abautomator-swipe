dbbf35c0f6a3818aa6591663f34d99793cf8913f
Rubens Flam-Shepherd <ruben.flam-shepherd@citizen.com>
2021-12-24 15:35:57 -0500

    rebuilding env, joining user to metric locally

--- abautomator/main.py
+++ abautomator/main.py
@@ -1,7 +1,8 @@
 from datetime import date
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from google.cloud import bigquery
+import sqlalchemy
 from sqlalchemy.engine import *
 from sqlalchemy.schema import Table, MetaData
 from sqlalchemy.sql import func, select
@@ -9,74 +10,68 @@ from sqlalchemy.sql import func, select
 from abautomator.config import GCP_PROJECT_ID
 
 
-
 from typing import List
-
-@dataclass
-class Cohort:
-  control: str
-  treatment_groups: List[str]
-  event: str
-  event_property: str
 
 
 @dataclass
 class Experiment:
-  cohort: Cohort
-  start: date
-  end: date=None
-  devices: List[str]=["android", "ios"]
+  ctrl_cond: str           # col values
+  tx_conds: List[str]      # col values
+  event: str               # table
+  event_prop: str          # table col
+  start_dt: date
+  end_dt: date=None
+  devices: List[str]=field(default_factory=lambda: ['android', 'ios'])
+
+  def all_conds(self):
+    return [self.ctrl_cond] + self.tx_conds
 
 
-def get_users(engine, exp_config):
-  table = Table(f'echelon.{exp_config["COHORTING_TABLE"]}', MetaData(bind=engine), autoload=True)
+def get_users_query(engine, exp):
+  table = Table(f'echelon.{exp.event}', MetaData(bind=engine), autoload=True)
 
   result = select(
-      table.c.echelon_user_id, 
-      getattr(table.c, exp_config["COHORTING_PROP"]).label("cohort"),
+      table.c.echelon_user_id,
+      getattr(table.c, exp.event_prop).label("cohort"),
   ).where(
-      getattr(table.c, exp_config["COHORTING_PROP"]).in_(exp_config["COHORT_NAMES"])
+      getattr(table.c, exp.event_prop).in_(exp.all_conds())
   ).group_by(
       table.c.echelon_user_id, 
-      getattr(table.c, exp_config["COHORTING_PROP"]),
+      getattr(table.c, exp.event_prop),
   )
-
   # Ommitting first_event_datetime for now
 
-  result = add_time_frame(result, table, exp_config)
+  result = add_time_frame(result, table, exp.start_dt, exp.end_dt)
+  return result
+
+@dataclass
+class Metric:
+  table_name: str
+  table_col: str
+
+def get_metric_query(engine, exp, metric):
+  table = Table(f'echelon.{metric.table_name}', MetaData(bind=engine), autoload=True)
+
+  result = select(
+      table.c.echelon_user_id,
+      func.count(getattr(table.c, metric.table_col)).label("n_events")
+  ).group_by(
+      table.c.echelon_user_id,
+  )
+  result = add_time_frame(result, table, exp.start_dt, exp.end_dt)
 
   return result
 
-def add_time_frame(query, table, exp_config):
-  if exp_config["EXP_START"]:
+def add_time_frame(query, table, start_dt=None, end_dt=None):
+  if start_dt:
     query = query.where(
-      table.c.event_date >= exp_config["EXP_START"]
+      table.c.event_date >= start_dt
     )
-  if exp_config["EXP_END"]:
+  if end_dt:
     query = query.where(
-      table.c.event_date <= exp_config["EXP_END"]
+      table.c.event_date <= end_dt
     )
   return query
 
-class Event:
-  def __init__(self, table_name, col):
-    self.table_name = table_name
-    self.col = col
-
-class EventMetric:
-  def __init__(self, event, exp_config):
-    self.event = event
-    self.exp_config = exp_config
-  
-  def get_query(self, conn):
-    table = Table(f'echelon.{self.event.table_name}', MetaData(bind=engine), autoload=True)
-
-    result = select(
-        table.c.echelon_user_id,
-        func.count(getattr(table.c, self.event.col).distinct()).label("n_events")
-    ).group_by(
-        table.c.echelon_user_id,
-    )
-    result = add_time_frame(result, table, self.exp_config)
-
-    return result
+def get_user_metrics_df(users_df, metric_df):
+  return users_df.join(metric_df, on="echelon_user_id", how="left")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,12 @@
 from datetime import date, timedelta
+
+import pandas as pd
+import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.schema import Table, MetaData
 from sqlalchemy.sql import select, selectable
 
-import pytest
+
 
 from abautomator import main, config
 
@@ -13,48 +16,87 @@ def engine(scope="module"):
     return create_engine(f'bigquery://{main.GCP_PROJECT_ID}')
 
 @pytest.fixture
-def connection(engine):
+def conn(engine):
     return engine.connect()
 
-def exp_config():
-    return {
-        "EXP_START": date(2021, 12, 10),
-        "EXP_END": None,
-        "DEVICES": ["android", "ios"],
-        "COHORTING_PROP": "context_traits_onboarding_flow_001",
-        "COHORTING_TABLE": "segment_signup_flow_started",
-        "COHORT_NAMES": [
+def test_conn(engine, conn):
+    table = Table(f"echelon.dim_groups", MetaData(bind=engine), autoload=True)
+
+    query = select(table).where(
+        getattr(table.c, "event_date") == _get_yesterday()
+    )
+
+    result = conn.execute(query).all()
+
+    assert len(result) > 10
+
+def _get_yesterday():
+    return date.today() - timedelta(days=1)
+
+@pytest.fixture
+def users_query(engine, exp):
+    return main.get_users_query(engine, exp)
+
+@pytest.fixture
+def exp():
+    return main.Experiment(
+        ctrl_cond='Dec1021InspirationMomentFinalControl',
+        tx_conds=[
             'Dec1021InspirationMomentFinalVideo01',
             'Dec1021InspirationMomentFinalVideo02',
             'Dec1021InspirationMomentFinalCarousel01',
             'Dec1021InspirationMomentFinalCarousel02',
             'Dec1021InspirationMomentFinalCarousel03',
             'Dec1021InspirationMomentFinalCarousel04',
-            'Dec1021InspirationMomentFinalControl',
         ],
-    }
+        event="segment_signup_flow_started",
+        event_prop="context_traits_onboarding_flow_001",
+        start_dt=_get_yesterday(),
+    )
 
-def get_yesterday():
-    return date.today() - timedelta(days=1)
+def test_get_users_query(conn, users_query):
+    assert isinstance(users_query, selectable.Select)
 
-# def test_connection(engine, connection):
-#     table = Table(f"echelon.dim_groups", MetaData(bind=engine), autoload=True)
+    result = conn.execute(users_query).all()
+    print(len(result))
+    assert len(result) > 10
 
-#     query = select(table).where(
-#         getattr(table.c, "event_date") == get_yesterday()
-#     )
+@pytest.fixture
+def metric_query(engine, exp):
+    return main.get_metric_query(engine, exp, get_session_metric())
 
-#     result = connection.execute(query).all()
+def get_session_metric():
+    return main.Metric(
+        table_name="fct_user_sessions",
+        table_col="id",
+    )
 
-#     assert len(result) > 10
 
-def test_get_users(engine, connection):
-    query = main.get_users(engine, exp_config())
-    assert isinstance(query, selectable.Select)
+def test_get_metric_query(conn, metric_query):
+    assert isinstance(metric_query, selectable.Select)
 
-    result = connection.execute(query).all()
+    result = conn.execute(metric_query).all()
+    print(len(result))
     assert len(result) > 10
 
 
 
+def test_get_users_metrics_df(conn, metric_query, users_query):
+    users_cte = users_query.cte("users")
+    session_metric_cte = metric_query.cte("sessions")
+    user_metrics_query = select(
+        users_cte.c.echelon_user_id, users_cte.c.cohort, session_metric_cte.c.n_events
+        ).select_from(
+            users_cte.join(session_metric_cte, users_cte.c.echelon_user_id == session_metric_cte.c.echelon_user_id, isouter=True)
+    )
 
+    # result = conn.execute(user_metrics_query).all()
+    # print(len(result))
+    # assert len(result) > 10
+
+    df = pd.read_sql(users_query, conn)
+    print(df.head())
+
+    assert len(df) > 10
+
+    # result = conn.execute(users_query).all()
