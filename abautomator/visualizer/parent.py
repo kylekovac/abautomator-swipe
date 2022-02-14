3460d18a6b9d0ecbe45d85d15690216f40478e04
Rubens Flam-Shepherd <ruben.flam-shepherd@citizen.com>
2022-02-13 19:40:57 -0500

    more strategic with how factor range category order is handeled

--- abautomator/visualizer/parent.py
+++ abautomator/visualizer/parent.py
@@ -6,19 +6,21 @@ from abautomator.visualizer import core, sig, btn, utils, bars
 class Visualizer:
     """ Parent object. Not to be initiated directly """
 
-    def __init__(self, cat_order, x_axis_label, source):
-        self.cat_order = cat_order
+    def __init__(self, x_axis_label, source):
         self.source = source
         self.x_axis_label = x_axis_label
 
     def get_figure(self):
-        fig = utils.init_fig(self.cat_order, self._get_tool_tips())
+        fig = utils.init_fig(self._get_y_range(), self._get_tool_tips())
         
         bars.add_core_bars(fig, self.source)
 
         self._setup_fig(fig)
         
         return fig
+    
+    def _get_y_range(self):
+        return list(self.source.data["factor_label"])
     
     def _setup_fig(self, fig):
         utils.add_zero_span(fig)
@@ -34,7 +36,7 @@ class StatSigVisualizer(Visualizer):
 
     def get_figure(self):
 
-        fig = utils.init_fig(self.cat_order, self._get_tool_tips())
+        fig = utils.init_fig(self._get_y_range(), self._get_tool_tips())
 
         bars.add_core_bars(fig, self.source)
         bars.add_sig_bars(fig, self.source)
