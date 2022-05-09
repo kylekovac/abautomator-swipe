c15810d3ff37b6773b0eddefa043da2e41dc62eb
Rubens Flam-Shepherd <ruben.flam-shepherd@citizen.com>
2022-05-09 01:08:30 +0100

    making the tab functionality better

--- app/app.py
+++ app/app.py
@@ -1,40 +1,22 @@
-import os
-import dill as pickle
+from flask import Flask, redirect, url_for
 
-from bokeh.embed import components
-from flask import Flask
-from flask import render_template
 
-from abautomator.visualizer import RelDiffVisualizer, AbsDiffVisualizer
 from abautomator.exp_config import PRIMARY_METRIC_LIST, SECONDARY_METRIC_LIST, GUARDRAIL_METRIC_LIST
 from app import utils
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/primary')
-def primary():
-    return render_specific_metrics(PRIMARY_METRIC_LIST, "primary metrics")
-
-
-def render_specific_metrics(metric_list, metric_label):
-
-    layouts = utils._get_layouts(RelDiffVisualizer, metric_list, metric_label)
-    scripts, divs = [], []
-    for layout in layouts:
-        bokeh_script, bokeh_div = components(layout)
-        scripts.append(bokeh_script)
-        divs.append(bokeh_div)
-    return render_template(
-        'index.html',
-        scripts=scripts,
-        divs=divs,
-        )
-
-@app.route('/secondary')
-def secondary():
-    return render_specific_metrics(SECONDARY_METRIC_LIST, "secondary metrics")
-
-@app.route('/guardrail')
-def guardrail():
-    return render_specific_metrics(GUARDRAIL_METRIC_LIST, "guardrail metrics")
+@app.route('/<metric_set>/')
+@app.route('/<metric_set>/<segment>')
+def get_graphs(metric_set="primary", segment=None):
+    if not segment:
+        return redirect(url_for("get_graphs", metric_set=metric_set, segment='all-charts'))
+    metric_mapping = {
+        'primary': PRIMARY_METRIC_LIST,
+        'secondary': SECONDARY_METRIC_LIST,
+        'guardrail': GUARDRAIL_METRIC_LIST,
+    }
+    if segment == "all-charts":
+        return utils.render_specific_metrics(metric_mapping[metric_set], f"{metric_set} metrics")
+    else:
+        return utils.render_specific_metrics(metric_mapping[metric_set], f"{metric_set} metrics", [segment])
